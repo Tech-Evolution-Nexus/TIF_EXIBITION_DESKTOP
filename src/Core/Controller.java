382180d67ea690f9ec4b32f6672867e1dd1a5199
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2024-03-23 22:22:33 +0700

    update core

---
+++ src/Core/Controller.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Core;
+
+import java.awt.Component;
+
+/**
+ *
+ * @author Muhammad Nor Kholit
+ */
+ public abstract class  Controller {
+    public abstract Component getView ();
+}
